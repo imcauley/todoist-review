6b0681f5c768e471aa8d1cf0b816f47150f7c7f8
Isaac McAuley <mcauleyisaac@gmail.com>
2024-02-02 19:16:22 -0500

    first version

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	MarkdownPostProcessorContext,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
@@ -7,103 +13,155 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	mySetting: "apiKey",
+};
+
+let apiKey = "";
+
+const createRequest = async () => {
+	const headers: Headers = new Headers();
+	headers.set("Content-Type", "application/json");
+	headers.set("Accept", "application/json");
+	headers.set("Authorization", "Bearer " + apiKey);
+
+	const request: RequestInfo = new Request(
+		"https://api.todoist.com/rest/v2/tasks?filter=overdue",
+		{
+			method: "GET",
+			headers: headers,
+		}
+	);
+
+	return fetch(request)
+		.then((res) => res.json())
+		.then((res) => {
+			return res;
+		});
+};
+
+const updateTask = async (id: string, postpone: string) => {
+	const headers: Headers = new Headers();
+	headers.set("Content-Type", "application/json");
+	headers.set("Accept", "application/json");
+	headers.set("Authorization", "Bearer " + apiKey);
+
+	const request: RequestInfo = new Request(
+		"https://api.todoist.com/rest/v2/tasks/" + id,
+		{
+			method: "POST",
+			headers: headers,
+			body: JSON.stringify({
+				due_string: postpone,
+			}),
+		}
+	);
+
+	return fetch(request)
+		.then((res) => res.json())
+		.then((res) => {
+			return res;
+		});
+};
+
+const changeButtonColor = (buttonID: string, id: string) => {
+	let button = document.getElementById(buttonID)!;
+
+	const accent = getComputedStyle(button).getPropertyValue("--color-accent");
+	const color = getComputedStyle(button).getPropertyValue(
+		"--interactive-normal"
+	);
+
+	["week", "tomorrow", "today"].forEach((b) => {
+		let current = document.getElementById(id + "-" + b + "-button")!;
+		current.style.backgroundColor = color;
+	});
+
+	button.style.backgroundColor = accent;
+};
+
+const buttonClicker = (buttonID: string, id: string, postpone: string) => {
+	return () => {
+		changeButtonColor(buttonID, id);
+		updateTask(id, postpone);
+	};
+};
+
+const convertTaskToHTML = (data: any): any => {
+	const main = document.createElement("div");
+	main.style.marginBottom = "1em";
+
+	const text = document.createElement("div");
+	text.style.marginBottom = "0.5em";
+	text.textContent = data["content"];
+	main.appendChild(text);
+
+	const week = document.createElement("button");
+	week.style.marginRight = "1.5em";
+	week.textContent = "Next Week";
+	const weekID = data["id"] + "-week-button";
+	week.id = weekID;
+	week.onClickEvent(buttonClicker(weekID, data["id"], "next week"));
+
+	const tomorrow = document.createElement("button");
+	tomorrow.style.marginRight = "1.5em";
+	tomorrow.textContent = "Tomorrow";
+	const tomorrowID = data["id"] + "-tomorrow-button";
+	tomorrow.id = tomorrowID;
+	tomorrow.onClickEvent(buttonClicker(tomorrowID, data["id"], "tomorrow"));
+
+	const today = document.createElement("button");
+	today.textContent = "Today";
+	const todayID = data["id"] + "-today-button";
+	today.id = todayID;
+	today.onClickEvent(buttonClicker(todayID, data["id"], "today"));
+
+	main.appendChild(week);
+	main.appendChild(tomorrow);
+	main.appendChild(today);
+	return main;
+};
+
+const codeProcessor = async (
+	source: string,
+	el: HTMLElement,
+	ctx: MarkdownPostProcessorContext
+) => {
+	const main = document.createElement("p");
+	const data = await createRequest();
+	data.map((d: any): any => main.appendChild(convertTaskToHTML(d)));
+
+	el.appendChild(main);
+	return;
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+		apiKey = this.settings.mySetting;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.registerMarkdownCodeBlockProcessor(
+			"todoist-review",
+			codeProcessor
+		);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -116,19 +174,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Todoist API Key")
+			.setDesc("")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
